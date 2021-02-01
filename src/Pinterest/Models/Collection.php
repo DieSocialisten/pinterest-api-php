77264ce25ae9d8551e0a0c7fdd9977ddc0ad23b9
Vladimir Posvistelik <v.posvistelik@gmail.com>
2021-02-01 12:47:55 +0300

    Fixes for Collection: wrong flag passed; added strict types mode; add one more type hint

--- src/Pinterest/Models/Collection.php
+++ src/Pinterest/Models/Collection.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * Copyright 2015 Dirk Groenen
- *
- * (c) Dirk Groenen <dirk@bitlabs.nl>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
+declare(strict_types=1);
 
 namespace DirkGroenen\Pinterest\Models;
 
@@ -176,7 +170,7 @@ class Collection implements JsonSerializable, ArrayAccess, IteratorAggregate
    */
   public function toJson(): string
   {
-    return json_encode($this->toArray(), true);
+    return json_encode($this->toArray());
   }
 
   /**
@@ -196,7 +190,7 @@ class Collection implements JsonSerializable, ArrayAccess, IteratorAggregate
    * @param mixed $offset
    * @return mixed
    */
-  public function offsetGet($offset)
+  public function offsetGet($offset): Model
   {
     return $this->items[$offset];
   }
